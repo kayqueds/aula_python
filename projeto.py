a2b8c6f85a34938f39689ef6da7623d2b930ad7a
Kayque Estevão <kayque2911@gmail.com>
2025-03-23 22:06:26 -0300

    Criando a pasta static

--- projeto.py
+++ projeto.py
@@ -1,54 +1,71 @@
-# pip install flask
-# faz a integração com páginas web
+# pip install flask bilbioteca
 
-# flask --version
+# importa biblioteca
+from flask import  Flask, render_template, request, redirect
 
-# a linha abaixo importa a bibliioteca Flask
-from flask import Flask, render_template
-
-# a linha abaixo cria uma classe
-
+# a linha baixo cria uma calsse
 class Aluno:
-    # o self serve para acessar as propriedade
-    # ela referencia a classe
     def __init__(self, ra, nome, idade, email):
         self.ra_aluno = ra
         self.nome_aluno = nome
         self.idade_aluno = idade
         self.email_aluno = email
-        
 
 
-# variável de aplicação para inicar
-app = Flask(__name__)
+ # as 3 linhas abaixo, instanciam alunos 
+aluno01 = Aluno('1001', 'Daniel Xavier', '32',
+                    'daniel@pro,fecaf.com.br')
+    
+aluno02 = Aluno('1002', 'Alex Silveira', '27', 
+                    'alex@a.fecaf.com.br')
+
+aluno03 = Aluno('1003', 'Raisa magalhaes', '22',
+                    'raisa@a.fecaf.com.br') 
+
+lista_alunos_cadastrados = [aluno01, aluno02, aluno03]
 
 
-# rota iniciaç
+app = Flask (__name__)
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# criando uma rota para a lista
-@app.route('/lista')
+
+
+@app.route("/lista")
 def lista_alunos():
+
     
-    # as 4 linhas abaixo, instanciam alunos
-    aluno01 = Aluno("1001", "Carlos Adão", "19", "carlosadao@gmail.com")
 
-    aluno02 = Aluno("1002", "Sérgio Figueiredo", "53", "sergiao@gmail.com")
+    return render_template("lista.html", titulo = 'UniFecaf', icone = '📝', 
+                           alunos = lista_alunos_cadastrados)
 
-    aluno03 = Aluno("1003", "Florinda Mesquita", "38", "florindames@gmail.com",)
+# a partir daqui eu trabalho com a tela cadastrar.html
+@app.route('/cadastrar')
+def cadastrar_aluno():
+    return render_template('cadastrar.html')
+# a rota/função abaixo, cadastra o aluno  na lista
+# e posteriormente no banco de dados
 
-    lista_alunos_cadastrados = [aluno01, aluno02, aluno03]
+@app.route('/add_aluno', methods=['POST',])
+def adicionar_aluno():
+    nome_recebido = request.form['txtNome']
+    idade_recebida = request.form['txtIdade']
+    email_recebeido = request.form['txtEmail']
+    ra_recebido = request.form['txtRA']
 
-   
+    # após pegar as informações do aluno
+    # devemos instaciar o aluno 
+    novo_aluno = Aluno(ra_recebido, nome_recebido,
+                       idade_recebida,email_recebeido)
+    
+    # a linha a baixo adiciona as informações do novo
+    # aluno na lista
+    lista_alunos_cadastrados.append(novo_aluno)
 
+    return redirect('/lista')
 
-    # variavel alunos vai armazenar a lista
-    return render_template('lista.html', titulo = 'FECAF',
-    alunos = lista_alunos_cadastrados)
-
-
-
-# esse comadando tem que ser executado no final
 app.run(debug=True)
+
+
