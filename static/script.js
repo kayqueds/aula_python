130ba465311055aa4a3b47d6bbb0274e7d1a0fda
Kayque Estevão <kayque2911@gmail.com>
2025-04-19 19:22:14 -0300

    Implementando bootstrap no arquivo

--- static/script.js
+++ static/script.js
@@ -3,12 +3,20 @@ function enviar(){
     let idade = document.getElementsByName('txtIdade')[0].value
     let email = document.getElementsByName('txtEmail')[0].value
 
-    let msg = "" 
     if (nome.trim() == "" | email.trim() == "" || idade.trim() == ""){
-        msg = 'ERRO, você não digitou todos os campos!'
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Preencha todos os campos!",
+
+          });
     }
     else{
-        msg = `Sucesso, bem-vindo ${nome}`
+        Swal.fire({
+            title: "Parabéns!",
+            text: "Seu cadastro foi enviado com sucesso!",
+            icon: "success"
+          });
     }
     alert(msg)
 }
